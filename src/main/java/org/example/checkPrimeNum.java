798a316f1b523e7f3ffd8f6823cce779df7e9a3a
Deepak kumar <deepakkumarsk014@gmail.com>
2025-01-28 23:06:51 +0530

    MiddleCharacter

--- src/main/java/org/example/checkPrimeNum.java
+++ src/main/java/org/example/checkPrimeNum.java
@@ -2,7 +2,7 @@ package org.example;
 
 import java.util.Scanner;
 
-public class checkPrimeNum {
+public class  checkPrimeNum {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         System.out.println("enter the number.");
